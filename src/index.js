4fe7082dfd81ea75c3fad9651e61fd7742adf27b
arondi-12 <davieariwi@gmail.com>
2024-10-15 21:32:15 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -30,29 +30,42 @@ async function getData() {
    if (runtimeElement){
     runtimeElement.innerText = film.runtime
    }
-//    const descriptionElement = document.getElementById("film-info")
-//    if (descriptionElement){
-//     descriptionElement.innerHTML = film.film-info 
-//     console.log(film.film-info)
-//    }
+   const descriptionElement = document.getElementById("film-info")
+   if (descriptionElement){
+    descriptionElement.innerText = film.description
    const showtimeElement = document.getElementById("showtime")
    if (showtimeElement){
-    showtimeElement.innerHTML = film.showtime
+    showtimeElement.innerText = film.showtime
    }
-//    const ticketnumElement = document.getElementById("ticket-num")
-//    if (ticketnumElement){
-//     ticketnumElement = film.ticket-num
-//    }
+   const ticketnumElement = document.getElementById("ticket-num")
+   if (ticketnumElement){
+    let ticketNum = film.capacity - film.tickets_sold
+    
+    ticketnumElement.innerText = ticketNum
+   }
+   const buyTicketBtn = document.getElementById("buy-ticket")
+    
+   if (buyTicketBtn) {
+    buyTicketBtn.onclick = () => buyTicket(film.id)
+   }
   }
 
   function updateFilmDetails(film) {
+    const poster = document.getElementById('poster');
+    const title = document.getElementById('title');
+    const runtime = document.getElementById('runtime');
+    const showtime = document.getElementById('showtime');
+    const filminfo = document.getElementById('film-info');
+    const ticketNum = document.getElementById('ticket-num');
+    const buyTicketButton = document.getElementById('buy-ticket');
+
     poster.src = film.poster;
     title.textContent = film.title;
     runtime.textContent = `${film.runtime} minutes`;
     showtime.textContent = film.showtime;
-    filmInfo.textContent = film.description;
+    filminfo.textContent = film.description;
     const availableTickets = film.capacity - film.tickets_sold;
-    ticketNum.textContent = `${availableTickets} remaining tickets`;
+    ticketNum.textContent = availableTickets;
     if (availableTickets === 0) {
         buyTicketButton.textContent = "Sold Out";
         buyTicketButton.disabled = true;
@@ -84,128 +97,4 @@ function buyTicket(filmId) {
             }
         });
 }
-// // Assuming this is your movie list data
-// let movies = [];
-
-// async function getData() {
-//   const url = "http://localhost:3000/films/";
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(Response status: ${response.status});
-//     }
-
-//     const json = await response.json();
-//     movies = json; // Store the fetched movies for later use
-//     const list = document.getElementById('films');
-//     list.innerHTML = json.map(i => 
-//       <li class="film item" onclick="updateUi(${i.id})">${i.title}</li>
-//     ).join('');
-//     console.log(json);
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-// getData();
-
-// function updateUi(movieId) {
-//   const film = movies.find(film => film.id == movieId); // Find the movie by id
-
-//   if (film) {
-//     console.log(film.poster);
-    
-//     const imageElement = document.getElementById("poster");
-//     if (imageElement) {
-//       imageElement.src = film.poster;
-//     }
-
-//     const titleElement = document.getElementById("title");
-//     if (titleElement) {
-//       titleElement.innerText = film.title;
-//     }
-
-//     const runtimeElement = document.getElementById("runtime");
-//     if (runtimeElement) {
-//       runtimeElement.innerText = ${film.runtime} minutes;
-//     }
-
-//     const showtimeElement = document.getElementById("showtime");
-//     if (showtimeElement) {
-//       showtimeElement.innerText = film.showtime;
-//     }
-//   } else {
-//     console.error("Movie not found!");
-//   }
-// } 
-
-
-// const filmTime = document.getElementById('film-item')
-// //declare the json url as an API for easy access.
-// const API = "http://localhost:3000/films";
-// fetch(API)
-//     .then((res) => res.json())
-//     .then(renderFilm)
-//const allMovies = renderFilm;
-//rendering only the one poster and its details.
-// function renderFilm(film) {
-//     const filmMenuDiv = document.getElementById("poster");
-//     const titleDiv = document.getElementById("film-info");
-//     const runDiv = document.getElementById("runtime")
-//     const showDiv = document.getElementById("showtime")
-//     const availableDiv = document.getElementById("ticket-num")
-//     filmMenuDiv.src = film.poster;
-//     titleDiv.textContent = film.title;
-//     runDiv.textContent = film.runtime;
-//     showDiv.textContent = film.showtime;
-//     const filmCap = film.capacity
-//     const filmTick = film.tickets_sold
-//     availableDiv.textContent = (filmCap - filmTick);
-//     console.log(availableDiv.textContent)
-//     //Buy tickets button
-//     const button = document.getElementById("buy-ticket");
-//     const btn = parseInt(availableDiv.innerText);
-//     console.log(btn);
-//     button.addEventListener('click', () => {
-//         if (btn >= 1) {
-//             availableDiv.innerText = (availableDiv.innerText - 1)
-//         }
-//         if (availableDiv.innerText < 0) {
-//             availableDiv.innerText = '0';
-//             alert('Sorry, we are currently sold out of tickets for this film.')
-//         }
-//         if (availableDiv.innerHTML <= 0) {
-//             button.innerHTML = 'SOLD OUT'
-//         }
-//     })
-// }
-// //Get the movie list.
-// function getFilms() {
-//     fetch(API)
-//         .then((response) => response.json())
-//         .then(renderFilms);
-// }
-// getFilms();
-// function renderFilms(films) {
-//     films.forEach(filmDetails); console.log(films)
-// }
-// function filmDetails(details) {
-//     const titlesElement = document.getElementById("films");
-//     let listElement = document.createElement("li");
-//     listElement.innerText = details.title;
-//     listElement.className = 'film-item'
-//     let imgElement = document.createElement("film-details");
-//     imgElement.src = details.poster;
-//     console.log(imgElement)
-//     const moviePoster = document.getElementById('poster')
-//     titlesElement.append(listElement);
-//     listElement.addEventListener('click', () => {
-//         console.log(details.title)
-//         document.getElementById('title').innerText = details.title
-//         document.getElementById('runtime').innerText = details.runtime
-//         document.getElementById('film-info').innerText = details.description
-//         document.getElementById('showtime').innerText = details.showtime
-//         document.getElementById('ticket-num').innerText = details.capacity - details.tickets_sold
-//         moviePoster.src = details.poster
-//     })
-// }
+  }
